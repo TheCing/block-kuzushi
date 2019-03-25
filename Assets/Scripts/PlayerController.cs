fae8e62a95bb97e28c5f7fbed437138f3824e8c7
JP Tyndall <jp.tyndall@luckie.com>
2019-03-25 14:03:49 -0400

    Add paddle and ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Camera camera;
+    SpriteRenderer spriteRenderer;
+    Vector3 spriteSize;
+    float spriteBoundLeft, spriteBoundRight;
+
+    public float moveSpeed;
+
+    float halfHeight, halfWidth, hMin, hMax, vMin, vMax;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+        halfHeight = camera.orthographicSize;
+        halfWidth = camera.aspect * halfHeight;
+
+        hMin = -halfWidth;
+        hMax =  halfWidth;
+        vMin = -halfHeight;
+        vMax = halfHeight;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteSize = spriteRenderer.bounds.size;
+        spriteBoundLeft = hMin + spriteSize.x / 2;
+        spriteBoundRight = hMax - spriteSize.x/2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        Vector3 step = new Vector3(h, 0, 0);
+        Vector3 nextPos = transform.position + (moveSpeed * step * Time.deltaTime);
+
+        if (nextPos.x < spriteBoundLeft)
+        {
+            nextPos.x = spriteBoundLeft;
+        }
+        else if (nextPos.x > spriteBoundRight)
+        {
+            nextPos.x = spriteBoundRight;
+        }
+
+        transform.position = nextPos;
+	}
+}
